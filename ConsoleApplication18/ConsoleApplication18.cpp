87d918036e3944faea15e16bc4a1e0d3ed34b1bc
Егор <@DESKTOP-VF4U8Q2>
2024-10-25 18:57:22 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication18/ConsoleApplication18.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int x = 7, y = 10;
+
+    while (true) {
+        cout << "Марсоход находится на позиции " << x << ", " << y << ", введите команду: ";
+        string command;
+        cin >> command;
+
+        if (command == "W") {
+            if (y < 20) {
+                y += 1;
+            }
+        }
+        else if (command == "S") {
+            if (y > 1) {
+                y -= 1;
+            }
+        }
+        else if (command == "A") {
+            if (x > 1) {
+                x -= 1;
+            }
+        }
+        else if (command == "D") {
+            if (x < 15) {
+                x += 1;
+            }
+        }
+        else {
+            cout << "Неверная команда" << endl;
+        }
+    }
